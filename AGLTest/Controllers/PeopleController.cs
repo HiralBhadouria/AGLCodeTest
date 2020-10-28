ad12e4b7272654e841fcdb4e58ca7c30484449ab
HiralBhadouria <73539249+HiralBhadouria@users.noreply.github.com>
2020-10-28 22:09:29 +1100

    Add files via upload

---
+++ AGLTest/Controllers/PeopleController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AGL.BusinessLogic;
+using AGL.Dto;
+using AGL.Library;
+using AGL.Presentation.Models;
+
+namespace AGL.Presentation.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private IPeopleBusinessLogic _peopleBusinessLogic;
+
+        public PeopleController(IPeopleBusinessLogic peopleBusinessLogic)
+        {
+            _peopleBusinessLogic = peopleBusinessLogic;
+        }
+
+        /// <summary>
+        /// Gets a list of people from the business layer in the correct view structure.
+        /// </summary>
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var peopleResponse = await _peopleBusinessLogic.GetPeople();
+                if (peopleResponse.ResponseStatus == ResponseStatusEnum.Failure)
+                {
+                    peopleResponse.Errors.ForEach(error =>
+                    {
+                        ModelState.AddModelError("", error);
+                    });
+
+                    return View(new List<GenderViewModel>());
+                }
+                else
+                {
+                    var viewModel = peopleResponse.Data.Select(gender => new GenderViewModel
+                    {
+                        Gender = gender.Gender,
+                        Cats = gender.Cats.Select(cat => new CatViewModel
+                        {
+                            Name = cat.Name
+                        }).ToList()
+                    }).ToList();
+
+                    return View(viewModel);
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+                return View(new List<GenderViewModel>());
+            }
+        }
+
+        /// <summary>
+        /// Returns contact details from the razor view
+        /// </summary>
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AGLTest/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using AGL.BusinessLogic;
+using AGL.Presentation.Controllers;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Unity.Mvc3;
+using AGL.ExternalServices;
+
+namespace AGLTest
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            container.RegisterType<IController, PeopleController>();
+            container.RegisterType<IPeopleBusinessLogic, PeopleBusinessLogic>();
+            container.RegisterType<IPeopleExternalService, PeopleExternalService>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+
+            //Bootstrapper.Initialise();
+
+        }
+    }
+}
